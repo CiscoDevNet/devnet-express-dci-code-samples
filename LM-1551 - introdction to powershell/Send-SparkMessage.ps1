db3ad6da2d32a1667e02dd25899cd9fc33159c9d
John McDonough <jomcdono@cisco.com>
2017-08-07 17:24:50 -0400

    Updated PowerShell Mission Spark Message script

--- LM-1551 - introdction to powershell/Send-SparkMessage.ps1
+++ LM-1551 - introdction to powershell/Send-SparkMessage.ps1
@@ -27,17 +27,44 @@ param(
     [string] $SparkApiToken,
 
   [Parameter(Mandatory=$true,HelpMessage="Enter a Spark Room Name - put in double quotes `" please.")]
+  [AllowEmptyString()]
     [string] $SparkRoomName,
     
   [Parameter(Mandatory=$true,HelpMessage="Enter a message - put in double quotes `" please.")]
+  [AllowEmptyString()]
     [string] $SparkMessage
 );
+
 # Set up the Header
 
     $headers = @{"Authorization" = "Bearer $SparkApiToken"; "Content-Type" = "application/json"; "Acccept" = "application/json"}
   
-    $sparkRoomId = Invoke-RestMethod -Uri https://api.ciscospark.com/v1/rooms -Headers $headers | %{$_.items | ?{$_.title -eq $SparkRoomName} |  Select-Object id}
+    try {
+        # Get the Id of the Spark Room
+        if ($SparkRoomName.Length -gt 0) {
+            $sparkRoomId = Invoke-RestMethod -Uri https://api.ciscospark.com/v1/rooms -Headers $headers | %{$_.items | ?{$_.title -eq $SparkRoomName} |  Select-Object id}
+        } else {
+            Write-Host "Please specify a Spark Room."
+            exit
+        }
 
-    $sparkRoomId
-
-    Invoke-RestMethod -Uri https://api.ciscospark.com/v1/messages -Method POST -Headers $headers -Body $('{"roomId":"' + $sparkRoomId.id + '", "text": "' + $SparkMessage + '"}')
+        # Send a Message to the Spark Room
+        if ($sparkRoomId) {
+            if ($SparkMessage.Length -gt 0) {
+                $invokeRestResponse = Invoke-RestMethod -Uri https://api.ciscospark.com/v1/messages -Method POST -Headers $headers -Body $('{"roomId":"' + $sparkRoomId.id + '", "text": "' + $SparkMessage + '"}')
+                if ($invokeRestResponse.id) {
+                    Write-Host "The message was sucessfully sent"
+                }
+            } else {
+                Write-Host "Please specify a Message."
+                exit
+            }
+        } else {
+            Write-Host -ForegroundColor Red "Spark Room: `"$SparkRoomName`" was not found!"
+        }
+    } catch {
+        Write-Host "There seems to be a problem!"
+        Write-Host "StatusCode:" $_.Exception.Response.StatusCode.value__ 
+        Write-Host "StatusDescription:" $_.Exception.Response.StatusDescription
+        exit
+    }
