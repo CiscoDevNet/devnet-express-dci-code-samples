aa8981023080c8f2f54b2ed313b0d4e68ea8d28c
John McDonough <jomcdono@cisco.com>
2017-08-11 18:06:02 -0400

    Update Script to send Spark Message for Mission Completion

--- LM-5401 - programming cisco compute/SendSparkMessage.py
+++ LM-5401 - programming cisco compute/SendSparkMessage.py
@@ -3,11 +3,11 @@ import sys
 import requests
 
 spark_api_token = 'YOUR TOKEN'
-spark_room_title = 'YOUR EVENT ROOM NAME'
+spark_room_name = 'YOUR EVENT ROOM NAME'
 spark_message = 'YOUR MESSAGE'
 #spark_message = 'I have completed the Programming Cisco Compute - Python Mission'
 
-if spark_api_token == 'YOUR TOKEN' or spark_room_title == 'YOUR EVENT ROOM NAME' or spark_message == 'YOUR MESSAGE':
+if spark_api_token == 'YOUR TOKEN' or spark_room_name == 'YOUR EVENT ROOM NAME' or spark_message == 'YOUR MESSAGE':
     print('Make the updates to the Spark variables')
     quit()
 
@@ -21,7 +21,7 @@ resp = requests.get(spark_uri_v1 + spark_rooms_resource, headers=spark_headers)
 resp = resp.json()
 
 for room in resp["items"]:
-     if room["title"] == spark_room_title:
+     if room["title"] == spark_room_name:
              spark_room_id = room["id"]
 
 spark_msg_json = {"roomId": spark_room_id, "text": spark_message}
